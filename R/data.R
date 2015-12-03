0e50653ee17da9a51074e874d37596ce17cd352f
Matthew Lincoln <matthew.d.lincoln@gmail.com>
2015-12-02 20:11:50 -0500

    data description

---
+++ R/data.R
@@ -0,0 +1,13 @@
+#' Historical Europe Population Data
+#'
+#' @format A data frame with 2653 rows and 4 columns:
+#' \describe{
+#'  \item{city}{City name (in native language)}
+#'  \item{region}{Region that city belongs to. One of the following: Sacndinavia, England and Wales, Scotland, Ireland, The Netherlands, Belgium, Germany, France, Switzerland, Northern Italy, Central Italy, Southern Italy, Spain, Portugal, Austria and Czechoslovakia, Poland}
+#'  \item{year}{}
+#'  \item{population}{Population in the tens of thousands. 0 represents a population under 10,000, while NA represent unknown values.}
+#' }
+"europop"
+
+#' Coodinates of European Cities
+"city_coords"
