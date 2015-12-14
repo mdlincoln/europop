7a98b511856d02e2bf823afd26e80869789a914d
Matthew Lincoln <matthew.d.lincoln@gmail.com>
2015-12-14 09:37:56 -0500

    clarify thousands

--- R/data.R
+++ R/data.R
@@ -1,13 +1,15 @@
 #' Historical Population Data for Urban Europe
 #'
+#' All cities that had a population of at least 10,000 at some point between 1500-1800 are included.
+#'
 #' @format A data frame with 2653 rows and 4 columns:
 #' \describe{
 #'  \item{\code{city}}{City name (in native language)}
 #'  \item{\code{region}}{Region that city belongs to. One of the following: Sacndinavia, England and Wales, Scotland, Ireland, The Netherlands, Belgium, Germany, France, Switzerland, Northern Italy, Central Italy, Southern Italy, Spain, Portugal, Austria and Czechoslovakia, Poland}
 #'  \item{\code{year}}{}
-#'  \item{\code{population}}{Population in the tens of thousands. 0 represents a population under 10,000, while NA is used when the value is unknown.}
+#'  \item{\code{population}}{Population in the thousands. 0 represents a population under 1,000, while NA is used when the value is unknown.}
 #' }
-#' @details This table can be joined to the \link{city_coords} table by the \code{city} column.
+#' @note This table can be joined to the \link{city_coords} table by the \code{city} column.
 #' @source Jan De Vries, \emph{European Urbanization, 1500-1800} (Cambridge: Harvard University Press, 1984), Appendix I.
 "europop"
 
@@ -19,5 +21,5 @@
 #'  \item{\code{lon}}{}
 #'  \item{\code{city}}{City name}
 #' }
-#' @details This table can be joined to the \link{europop} table by the \code{city} column.
+#' @note This table can be joined to the \link{europop} table by the \code{city} column.
 "city_coords"
