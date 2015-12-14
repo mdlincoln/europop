6e953370bc05f42b9ec2c36be557e36170831c31
Matthew Lincoln <matthew.d.lincoln@gmail.com>
2015-12-14 09:18:28 -0500

    Improve formatting

--- R/data.R
+++ R/data.R
@@ -2,10 +2,10 @@
 #'
 #' @format A data frame with 2653 rows and 4 columns:
 #' \describe{
-#'  \item{city}{City name (in native language)}
-#'  \item{region}{Region that city belongs to. One of the following: Sacndinavia, England and Wales, Scotland, Ireland, The Netherlands, Belgium, Germany, France, Switzerland, Northern Italy, Central Italy, Southern Italy, Spain, Portugal, Austria and Czechoslovakia, Poland}
-#'  \item{year}{}
-#'  \item{population}{Population in the tens of thousands. 0 represents a population under 10,000, while NA is used when the value is unknown.}
+#'  \item{\code{city}}{City name (in native language)}
+#'  \item{\code{region}}{Region that city belongs to. One of the following: Sacndinavia, England and Wales, Scotland, Ireland, The Netherlands, Belgium, Germany, France, Switzerland, Northern Italy, Central Italy, Southern Italy, Spain, Portugal, Austria and Czechoslovakia, Poland}
+#'  \item{\code{year}}{}
+#'  \item{\code{population}}{Population in the tens of thousands. 0 represents a population under 10,000, while NA is used when the value is unknown.}
 #' }
 #' @source Jan De Vries, \emph{European Urbanization, 1500-1800} (Cambridge: Harvard University Press, 1984), Appendix I.
 "europop"
