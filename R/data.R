2f24f146be52d625eb9f4eb3b43d3ff467520f97
Matthew Lincoln <matthew.d.lincoln@gmail.com>
2017-01-17 09:05:42 -0800

    refined documentation
    - Fixed some typos
    - Itemized the city region list
    - Wrapped some lines

--- R/data.R
+++ R/data.R
@@ -1,25 +1,49 @@
 #' Historical Population Data for Urban Europe
 #'
-#' All cities that had a population of at least 10,000 at some point between 1500-1800 are included.
+#' All cities that had a population of at least 10,000 at some point between
+#' 1500-1800 are included.
 #'
 #' @format A data frame with 2653 rows and 4 columns:
 #' \describe{
-#'  \item{\code{city}}{City name (in native language)}
-#'  \item{\code{region}}{Region that city belongs to. One of the following: Scandinavia, England and Wales, Scotland, Ireland, The Netherlands, Belgium, Germany, France, Switzerland, Northern Italy, Central Italy, Southern Italy, Spain, Portugal, Austria and Czechoslovakia, Poland}
-#'  \item{\code{year}}{}
-#'  \item{\code{population}}{Population in the thousands. 0 represents a population under 1,000, while NA is used when the value is unknown.}
+#'  \item{\code{city}}{Character. City name (in native language.)}
+#'  \item{\code{region}}{Character. Region that city belongs to. One of the
+#'  following: \itemize{
+#'  \item Scandinavia
+#'  \item England and Wales
+#'  \item Scotland
+#'  \item Ireland
+#'  \item The Netherlands
+#'  \item Belgium
+#'  \item Germany
+#'  \item France
+#'  \item Switzerland
+#'  \item Northern Italy
+#'  \item Central Italy
+#'  \item Southern Italy
+#'  \item Spain
+#'  \item Portugal
+#'  \item Austria and Czechoslovakia
+#'  \item Poland}}
+#'  \item{\code{year}}{Integer.}
+#'  \item{\code{population}}{Integer. Population in the thousands. 0 represents
+#'  a population under 1,000, while NA is used when the value is unknown.}
 #' }
-#' @note This table can be joined to the \link{city_coords} table by the \code{city} column.
-#' @source Jan De Vries, \emph{European Urbanization, 1500-1800} (Cambridge: Harvard University Press, 1984), Appendix I.
+#'
+#' @note This table can be joined to the \link{city_coords} table by the
+#'   \code{city} column.
+#'
+#' @source Jan De Vries, \emph{European Urbanization, 1500-1800} (Cambridge:
+#'   Harvard University Press, 1984), Appendix I.
 "europop"
 
 #' Coodinates of European Cities
 #'
-#' @format A data frame with 379 rows and 3 colums
+#' @format A data frame with 379 rows and 3 columns
 #' \describe{
-#'  \item{\code{lat}}{}
-#'  \item{\code{lon}}{}
-#'  \item{\code{city}}{City name}
+#'  \item{\code{lat}}{Double. Latitude.}
+#'  \item{\code{lon}}{Double. Longitude.}
+#'  \item{\code{city}}{Character. City name.}
 #' }
-#' @note This table can be joined to the \link{europop} table by the \code{city} column.
+#' @note This table can be joined to the \link{europop} table by the \code{city}
+#'   column.
 "city_coords"
